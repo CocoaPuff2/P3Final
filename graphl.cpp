19bb0c19741c8d3e832037f42530f36a936f8c13
Sana (Q) <@CocoaPuff2>
2025-02-06 23:49:52 -0800

    working on part 2

--- graphl.cpp
+++ graphl.cpp
@@ -1,3 +1,57 @@
-//
-// Created by Qasim Zuhair on 2/4/25.
-//
+#include "graphl.h"
+#include <iostream>
+#include <fstream>
+#include <climits>
+#include <string>
+
+using namespace std;
+
+GraphL::GraphL() {
+    size = 0;
+    nodeArray = nullptr;
+}
+
+GraphL::~GraphL() {
+    // free the memory used by nodes and edges if (nodeArray != nullptr) {
+    if (nodeArray != nullptr) {
+        //  delete its edge list (if any) for each node, starting with head
+        for (int i = 0; i < size; i++) {
+            EdgeNode* edge = nodeArray[i].edgeHead;
+            // traverse thru LL of nodes
+            while (edge != nullptr) {
+                EdgeNode* temp = edge; // store current node edge
+                edge = edge->nextEdge; // move to next node
+                delete temp; // free mem
+            }
+        }
+        delete[] nodeArray; // dealloc entire array
+    }
+
+}
+
+//  Reads graph data from input and builds the graph structure
+// 1. 1st line of input gives # of  nodes in the graph
+// 2. description of each node in next lines & node's edges
+// 3. allocate memory for the nodeArray based on # nodes & populate nodeArray with data
+// 4.  Each node will have its edge list constructed dynamically.
+int GraphL::buildGraph(ifstream& input) {
+    input >> size;
+    if (size <= 0) {
+        return -1;
+    }
+
+    // alloc mem for nodeArray (array that contains GraphNodes)
+    nodeArray = new GraphNode[size];
+    for (int i = 0; i < size; i++) {
+        nodeArray[i].edgeHead = nullptr;
+        nodeArray[i].visited = false;
+        getline(input, nodeArray[i].data);
+    }
+}
+
+void GraphL::displayGraph() const {
+
+}
+void GraphL::depthFirstSearch() const {
+
+}
--- graphl.h
+++ graphl.h
@@ -1,8 +1,47 @@
-//
-// Created by Qasim Zuhair on 2/4/25.
-//
-
 #ifndef MY_EXECUTABLE_GRAPHL_H
 #define MY_EXECUTABLE_GRAPHL_H
+
+#include <iostream>
+#include <fstream>
+#include <climits>
+#include <string>
+
+using namespace std;
+
+
+struct EdgeNode;      // forward reference for the compiler
+
+struct GraphNode {    // structs used for simplicity, use classes if desired
+    EdgeNode* edgeHead; // head of the list of edges
+    string data;     // data information about each node
+    bool visited;
+
+};
+
+
+
+struct EdgeNode {
+    int adjGraphNode;  // subscript of the adjacent graph node
+    EdgeNode* nextEdge;
+};
+
+
+class GraphL {
+public:
+    GraphL();
+    ~GraphL();
+    int buildGraph(ifstream& input);
+    void displayGraph() const;
+    void depthFirstSearch() const;
+
+
+private:
+    // array of GraphNodes
+    GraphNode* nodeArray; // array for GraphNodes
+    int size; // # of nodes
+    void dfsHelper(int nodeIndex) const;
+
+};
+
 
 #endif //MY_EXECUTABLE_GRAPHL_H
